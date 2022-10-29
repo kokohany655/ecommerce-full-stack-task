8a4e4b3a6291992fe21676d6e010d92da81703a2
kokohany655 <https://github.com/kokohany655>
2022-10-29 04:54:26 +0200

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="layout">
       <NavBar className='header'/>
-      <Container className="main">
-
+      <Container maxW={{xl : 'container.xl' , lg:'container.lg' , md:'container.md' , sm:'container.sm'}} className="main">
+        <Product/>
       </Container>
       <Footer className='footer'/>
     </div>
