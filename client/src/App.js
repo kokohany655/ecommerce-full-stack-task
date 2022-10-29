feaa84ccebc8396f9b2e6e08a674aafb22b1e854
kokohany655 <https://github.com/kokohany655>
2022-10-29 03:53:21 +0200

    footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from "@chakra-ui/react";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Container >
+    <div className="layout">
+      <NavBar className='header'/>
+      <Container className="main">
 
       </Container>
+      <Footer className='footer'/>
     </div>
   );
 }
